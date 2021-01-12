875028d32e951bed27e7dbe1f11bbef07560afaa
aamdo <a0555170767@gmail.com>
2021-01-12 22:15:25 +0300

    12

--- lms_apps/lms_app/urls.py
+++ lms_apps/lms_app/urls.py
@@ -3,6 +3,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home ,name='home'),
+    path('',views.index ,name='index'),
+    path('books/',views.index ,name='books'),
+    path('delete/',views.index ,name='delete'),
+    path('update/',views.index ,name='update'),
     
 ]
--- lms_apps/lms_app/views.py
+++ lms_apps/lms_app/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
+def index(request):
     context = {}
-    return render(request,'lms/home.html',context)
+    return render(request,'lms/index.html',context)
+
+def books(request):
+    context = {}
+    return render(request,'lms/books.html',context)
+
+def delete(request):
+    context = {}
+    return render(request,'lms/delete.html',context)
+
+def update(request):
+    context = {}
+    return render(request,'lms/update.html',context)
 
