5eadc1a85a047509268adea25509fb02171bab58
aamdo <a0555170767@gmail.com>
2021-01-18 14:09:30 +0300

    19----8:0

--- lms_apps/lms_app/views.py
+++ lms_apps/lms_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from.forms import BookForm
 
 # Create your views here.
 
@@ -7,6 +8,7 @@ def index(request):
     context = {
         'categories':Category.objects.all(),
         'books':Book.objects.all(),
+        'form': BookForm(),
         }
     return render(request,'lms/index.html',context)
 
