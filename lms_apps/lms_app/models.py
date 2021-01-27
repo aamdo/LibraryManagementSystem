3a96e2f4ad73aad6677d332d617781a6199f862c
aamdo <a0555170767@gmail.com>
2021-01-27 14:14:44 +0300

    28

--- lms_apps/lms_app/models.py
+++ lms_apps/lms_app/models.py
@@ -11,29 +11,33 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 class Book(models.Model):
 
     book_status = {
-        ('availble','availble'),
-        ('rented','rented'),
-        ('sold','sold'),
-        }
-    
+        ('availble', 'availble'),
+        ('rented', 'rented'),
+        ('sold', 'sold'),
+    }
+
     title = models.CharField(max_length=250)
-    author = models.CharField(max_length=250,blank=True, null=True)
-    photo_book = models.ImageField(upload_to='photo',blank=True, null=True)
-    photo_author = models.ImageField(upload_to='photo',blank=True, null=True)
+    author = models.CharField(max_length=250, blank=True, null=True)
+    photo_book = models.ImageField(upload_to='photo', blank=True, null=True)
+    photo_author = models.ImageField(upload_to='photo', blank=True, null=True)
     pages = models.IntegerField(blank=True, null=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2,blank=True, null=True)
-    rental_price_day=models.DecimalField(max_digits=5, decimal_places=2,blank=True, null=True)
-    rental_period=models.IntegerField(blank=True, null=True)
-    total_rental=models.DecimalField(max_digits=5, decimal_places=2,blank=True, null=True)
-    active=models.BooleanField(blank=True, null=True)
-    status = models.CharField(max_length=50,choices=book_status,blank=True, null=True)
-    category=models.ForeignKey(Category,on_delete=PROTECT,blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=5, decimal_places=2, blank=True, null=True)
+    rental_price_day = models.DecimalField(
+        max_digits=5, decimal_places=2, blank=True, null=True)
+    rental_period = models.IntegerField(blank=True, null=True)
+    total_rental = models.DecimalField(
+        max_digits=5, decimal_places=2, blank=True, null=True)
+    active = models.BooleanField(blank=True, null=True)
+    status = models.CharField(
+        max_length=50, choices=book_status, blank=True, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=PROTECT, blank=True, null=True)
 
     def __str__(self):
         return self.title
-    
